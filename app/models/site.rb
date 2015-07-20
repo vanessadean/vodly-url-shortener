6a98b193d9779ea78f9c8a40289f557804227979
Vanessa Dean <vanessa.dean@gmail.com>
2015-07-20 09:08:32 -0400

    Update url shortener algorithm, README and tests

--- app/models/site.rb
+++ app/models/site.rb
@@ -5,14 +5,18 @@ class Site < ActiveRecord::Base
     self.short_url_code = url_maker(self.id)
   end
 
-  private
   def url_maker(id)
-    chars = ("a".."z").to_a + ("1".."9").to_a
-    url = chars[id%35]
-    n = Math.log(id, 35).floor
+    @chars = get_chars
+    @base = @chars.length #62
+    url = @chars[id % @base]
+    n = Math.log(id, @base).floor
     n.times do 
-      url += chars[id/35**n]
+      url += @chars[id/@base**n]
     end
     url
+  end
+
+  def get_chars
+    ("a".."z").to_a + ("A".."Z").to_a + ("0".."9").to_a
   end
 end
--- test/models/site_test.rb
+++ test/models/site_test.rb
@@ -9,10 +9,31 @@ class SiteTest < ActiveSupport::TestCase
     @site2 = Site.create(original_url: "http://vanessadean.github.io/")
     @site2.set_short_code
     @site2.save
+
+    @site3 = Site.new(original_url: "http://vanessadean.github.io/")
   end
 
   test "set short code should build a unique url" do
     assert @site.short_url_code == "b"
     assert @site2.short_url_code == "c"
+  end
+
+  test "set short code should build a one digit url for ids < 62" do
+    url2 = @site3.url_maker(1)
+    assert url2.length == 1
+    url2 = @site3.url_maker(61)
+    assert url2.length == 1
+  end    
+
+  test "set short code should build a two digit url for ids between 62 and 3844" do
+    url2 = @site3.url_maker(62)
+    assert url2.length == 2
+    url2 = @site3.url_maker(3833)
+    assert url2.length == 2
+  end
+
+  test "set short code should build a three digit url for ids >= 3844" do
+    url2 = @site3.url_maker(3844)
+    assert url2.length == 3
   end
 end
