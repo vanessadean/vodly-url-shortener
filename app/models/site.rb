db15968f660f6812bf4c44bd3113dcc202ef27af
Vanessa Dean <vanessa.dean@gmail.com>
2015-07-21 17:42:09 -0400

    Fix url bug

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,5 +1,6 @@
 class Site < ActiveRecord::Base
   validates :original_url, presence: true
+  after_save :validate_url
 
   def set_short_code
     self.short_url_code = url_maker(self.id)
@@ -18,5 +19,13 @@ class Site < ActiveRecord::Base
 
   def get_chars
     ("a".."z").to_a + ("A".."Z").to_a + ("0".."9").to_a
+  end
+
+  private
+  def validate_url
+    if !original_url.match(/^http:\/\//)
+      self.original_url = "http://#{original_url}"
+      self.save
+    end
   end
 end
